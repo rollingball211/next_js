a8db27f24e3fcfd33ae9d41e24dee6876f98f295
rollingball211 <jingoo211@gmail.com>
2025-04-16 00:09:48 +0900

    books ui final

--- section2/src/pages/book/[id].tsx
+++ section2/src/pages/book/[id].tsx
@@ -1,3 +1,4 @@
+import style from "./[id].module.css"
 const mockData = 
     {
       "id": 1,
@@ -20,8 +21,22 @@ export default function Page() {
         coverImgUrl
     } = mockData;
     
-    return (<div>
-        <div style={{backgroundImage:`url('${coverImgUrl}')`}}></div>    
+    return (
+    <div className={style.container}>
+        <div 
+            className={style.cover_img_container}
+            style={{ backgroundImage:`url('${coverImgUrl}')`}}
+        >
+            <img src= {coverImgUrl} />        
+        </div>
+
+        <div className={style.title}> {title} </div>
+        <div className={style.subTitle}> {subTitle} </div>
+        <div className={style.author}> 
+            {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
+
     </div>
     )
 }
