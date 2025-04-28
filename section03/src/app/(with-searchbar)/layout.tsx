b4f1cd1d4b0991101742e91aa4ffc002d23f2429
rollingball211 <jingoo211@gmail.com>
2025-04-28 14:19:03 +0900

    ClientComponent

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import SearchBar from "./searchbar";
 
 export default function Layout({children} : {
     children : ReactNode
 }){
     return ( 
     <div>
-        <div>임시 서치바</div>
+        <SearchBar/>
         {children}
     </div>  
     );
