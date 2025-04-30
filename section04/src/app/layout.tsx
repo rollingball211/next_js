54127074ae5261d35ffcb1cc2803739bfd9478e6
rollingball211 <jingoo211@gmail.com>
2025-04-30 20:42:07 +0900

    footer_component

--- section04/src/app/layout.tsx
+++ section04/src/app/layout.tsx
@@ -1,6 +1,21 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!response.ok) {
+    <footer>제작 @jinhyeon</footer>
+  }
+  const books : BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return <footer>
+    <div>제작 @jinhyeon</div>
+    <div>{bookCount} 권의 도서가 등록되어 있습니다. </div>
+  </footer>
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +30,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @jinhyeon</footer>
+          <Footer/>
         </div>
       </body>
     </html>
