51192b8c2d2d29a2161ff999039d8f336084dcb1
rollingball211 <jingoo211@gmail.com>
2025-04-30 19:38:39 +0900

    appdataFetch+md modfiy

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# next_js
+## 📆 Next.js 작업 기록
 
-03.05 ) chpater1
-03.07) 서버 연결 및 코드 수정 진행 중 08 업로드 예정
+- 2025.03.05: Rendering  
+- 2025.03.07~11: DB / Backend Setting / 프로젝트 에러 해결  
+- 2025.03.12: Page Router  
+- 2025.03.13~14: Navigation / Prefetch / API Routes / Styling  
+- 2025.03.18: Global Layout  
+- 2025.04.10: UI 구현  
+- 2025.04.11: 사전 렌더링  
+- 2025.04.17: Data Fetch / Next의 사전 렌더링  
+- 2025.04.18: SSR / SSG 구현  
+- 2025.04.21: ISR 구현 / SEO 설정  
+- 2025.04.22: Page Router 총정리  
+- 2025.04.23: App Router  
+- 2025.04.28: App Router / Layout  
+- 2025.04.29: App Router  
+- 2025.04.30~: 데이터 페칭 (TypeScript + Next.js) 진행 중
---
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,58 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+//모든 도서의 페이지를 사용하는 부분
+async function AllBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+  
+  const allBooks: BookData[] = await response.json();
+
+  //fetch 요청이 어떤식으로 들어올지 알 수 없음
+  //any 타입이 아닌 타입 정보를 줘야함
+  //() => () 면 리턴함수 필요 x 
+  return(
+   <div>  
+    {allBooks.map( (book) => (
+      <BookItem key ={book.id}{...book} />
+    ))}
+  </div>
+  );
+}
+
+//추천도서 부분
+async function RecoBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`)
+  if (!response.ok) {
+    return <div>오류가 발생헀습니다.</div>
+  }
+
+  const recoBooks : BookData[] = await response.json();
+  return (
+  <div>
+    {recoBooks.map( (book) => (
+      <BookItem key = {book.id}{...book}/>
+    ))}
+  </div>)
+}
+
+
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks/>
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks/>
+      </section>
+    </div>
+  );
+}
