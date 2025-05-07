af073fd7d3c1c884b6c7a59b7a7aec82e5edf5d9
rollingball211 <jingoo211@gmail.com>
2025-05-08 02:28:51 +0900

    dynamicPageCache

--- section05/src/app/book/[id]/page.tsx
+++ section05/src/app/book/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
-export const dynamicParams = false;
+//export const dynamicParams = false;
 
 export function generateStaticParams( ) {
   return [{id:"1"},{id:"2"},{id:"3"}];
