f053509742a621a518684e6ba6843c657a3cdbdc
rollingball211 <jingoo211@gmail.com>
2025-05-17 21:29:41 +0900

    Addreview_ServerAction

---
+++ section07/src/actions/create-review.action.ts
@@ -0,0 +1,31 @@
+"use server";
+
+export async function createReviewAction (formData : FormData) {
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+    const bookId  = formData.get("bookId")?.toString();
+
+
+    console.log(bookId,author,content);
+    //api 호출 
+    if ( !bookId || !content || !author) {
+      return;
+    } 
+
+    
+    
+    try{
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+        {
+          method:"POST",
+          body : JSON.stringify({bookId,content,author}), //객체를 직렬화해서 전달
+        }
+        );
+        console.log(response.status);
+
+    } catch(err) {
+      console.error(err);
+      return;
+    }    
+  }
