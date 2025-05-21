a7e0213783fba2845f4d685db4fad1b2de00345b
rollingball211 <jingoo211@gmail.com>
2025-05-21 18:50:58 +0900

    deleteReviews

--- section07/src/components/review-item.tsx
+++ section07/src/components/review-item.tsx
@@ -1,5 +1,6 @@
 import { ReviewData } from "@/types";
 import style from "./review-item.module.css";
+import ReviewItemDeleteButton from "./review-item-delete-button";
 
 export default function ReviewItem({
     id,
@@ -9,12 +10,17 @@ export default function ReviewItem({
     bookId,
 }: ReviewData) { //props의 타입은 ReviewData! 
     return(
+    
      <div className={style.container}>
         <div className={style.author}>{author}</div>
         <div className={style.content}>{content}</div>
         <div className={style.bottom_container}>
-            <div>{new Date(createdAt).toLocaleDateString()}</div>
-            <div className={style.delete_btn}>삭제하기</div>
+            <div className={style.date}>
+                {new Date(createdAt).toLocaleDateString()}
+            </div>
+            <div className={style.delete_btn}>
+                <ReviewItemDeleteButton reviewId={id} bookId={bookId} />
+            </div>
         </div>
     </div>
     );
