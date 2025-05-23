f26f85148b2480ea86fd3f2a850069fbe94ea6d4
rollingball211 <jingoo211@gmail.com>
2025-05-23 23:07:19 +0900

    intercept2

--- section08/src/app/(.)book/[id]/page.tsx
+++ section08/src/app/(.)book/[id]/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
-    return <div>가로채기 성공!</div>
+import BookPage from "@/app/book/[id]/page";
+export default function Page(props : any) {
+    return <div>가로채기 성공!
+        <BookPage {...props} />
+    </div>
 }
