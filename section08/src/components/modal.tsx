4ee47bf5553c51cdc6744cf1ef9f5a223c8efbd6
rollingball211 <jingoo211@gmail.com>
2025-05-26 22:11:05 +0900

    modal3

--- section08/src/components/modal.tsx
+++ section08/src/components/modal.tsx
@@ -2,9 +2,11 @@
 import { ReactNode, useEffect, useRef } from "react";
 import style from "./modal.module.css";
 import { createPortal } from "react-dom";
+import { useRouter } from "next/navigation";
 
 export default function Modal( {children} : {children : ReactNode}) {
     const dialogRef = useRef<HTMLDialogElement>(null);
+    const router = useRouter();
 
     //modal을 먼저 실행하는 코드
     //열려있지 않으면 모달을 강제로 열고 ,스크롤을 제일 위로 올림
@@ -17,7 +19,14 @@ export default function Modal( {children} : {children : ReactNode}) {
         }
     }, []);
     return createPortal(
-    <dialog className={style.modal} ref={dialogRef}>{children}</dialog>,
+    <dialog 
+    onClose={() => router.back()}
+    onClick={(e) => {
+        if((e.target as any ).nodeName === "DIALOG") {
+            router.back();
+        }
+    }}className={style.modal}
+     ref={dialogRef}>{children}</dialog>,
     document.getElementById('modal-root') as HTMLElement);
 
     //dialog 태그는 모달의 역할을 하기 때문에 꺼져있는 상태로 렌더링이 됨
